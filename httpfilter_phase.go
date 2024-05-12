230a556ba032ac81fafcccba4ad0794552eaa6ba
Ardika Bagus <me@ardikabs.com>
2024-05-12 18:40:07 +0700

    chore: add comment docs for clarity about the decision
    Signed-off-by: Ardika Bagus <me@ardikabs.com>

--- httpfilter_phase.go
+++ httpfilter_phase.go
@@ -30,7 +30,7 @@ func newDecodeHeadersPhase(header api.RequestHeaderMap) HttpFilterPhaseFunc {
 			return ActionContinue, err
 		}
 
-		if c.IsRequestBodyReadable() {
+		if c.IsResponseBodyWriteable() {
 			c.RequestHeader().Del(HeaderXContentOperation)
 			return ActionPause, nil
 		}
@@ -41,8 +41,7 @@ func newDecodeHeadersPhase(header api.RequestHeaderMap) HttpFilterPhaseFunc {
 
 func newDecodeDataPhase(buffer api.BufferInstance, endStream bool) HttpFilterPhaseFunc {
 	return func(c Context, d HttpFilterPhaseDirector) (HttpFilterAction, error) {
-		isRequestBodyAccessible := c.IsRequestBodyReadable() || c.IsRequestBodyWriteable()
-		if !isRequestBodyAccessible {
+		if !isRequestBodyAccessible(c) {
 			return ActionSkip, nil
 		}
 
@@ -66,6 +65,17 @@ func newEncodeHeadersPhase(header api.ResponseHeaderMap) HttpFilterPhaseFunc {
 			return ActionContinue, err
 		}
 
+		// During the Encode phases or HTTP Response flows,
+		// if a user needs access to the HTTP Response Body, whether for reading or writing,
+		// the EncodeHeaders phase should return with ActionPause (StopAndBuffer status) status.
+		// This is necessary because the Response Header must be buffered in Envoy's filter-manager.
+		// If this buffering is not done, the Response Header might be sent to the downstream client prematurely,
+		// preventing the filter from returning a custom error response in case of unexpected events during processing.
+		//
+		// Hence, we opted for the IsResponseBodyReadable check instead of IsResponseBodyWritable.
+		// It's worth noting that the behavior differs in the Decode phase because the stream flow is directed towards the upstream.
+		// This means that even if DecodeHeaders has returned with ActionContinue (Continue status),
+		// DecodeData is still under supervision within Envoy's filter-manager state.
 		if c.IsResponseBodyReadable() {
 			c.ResponseHeader().Del(HeaderXContentOperation)
 			return ActionPause, nil
@@ -77,8 +87,14 @@ func newEncodeHeadersPhase(header api.ResponseHeaderMap) HttpFilterPhaseFunc {
 
 func newEncodeDataPhase(buffer api.BufferInstance, endStream bool) HttpFilterPhaseFunc {
 	return func(c Context, d HttpFilterPhaseDirector) (HttpFilterAction, error) {
-		isResponseBodyAccessible := c.IsResponseBodyReadable() || c.IsResponseBodyWriteable()
-		if !isResponseBodyAccessible {
+		// Attention! Please be mindful of the Listener or Cluster per_connection_buffer_limit_bytes value
+		// when enabling the response body access on ConfigOptions (EnableResponseBodyRead or EnableResponseBodyWrite).
+		// The default value set by Envoy is 1MB. If the response body size exceeds this limit, the process will be halted.
+		// Although it's unclear whether this is considered a bug or a limitation at present,
+		// the Envoy Golang HTTP Filter library currently returns a 413 status code with a PayloadTooLarge message in such cases.
+		// Code references: https://github.com/envoyproxy/envoy/blob/v1.29.4/contrib/golang/filters/http/source/processor_state.cc#L362-L371.
+		//
+		if !isResponseBodyAccessible(c) {
 			return ActionSkip, nil
 		}
 
